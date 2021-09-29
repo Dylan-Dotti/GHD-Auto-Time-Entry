33023d432ac7d081698aa3642d4a1a6fdfeca454
Dylan-Dotti <invocation42@yahoo.com>
2021-09-28 23:27:14 -0400

    Removed files and reference related to building an exe

--- generate_exe.bat
+++
@@ -1,2 +0,0 @@
-if exist "%CD%\dist" rd /q /s "%CD%\dist"
-python generate_exe.py py2exe
--- generate_exe.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup
-import py2exe
-
-setup(console=['testing/auto_gui/test_auto_entry_agent.py'])
