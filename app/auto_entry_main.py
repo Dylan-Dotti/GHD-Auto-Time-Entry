b236bb64cfcbf473c1c9359fe96eba3562396686
Dylan-Dotti <invocation42@yahoo.com>
2021-09-29 12:32:44 -0400

    Setup AutoEntryMain to run on a separate thread
    Setup AutoEntryMain to run on a separate thread so that it no longer locks up the GUI

--- app/auto_entry_main.py
+++ app/auto_entry_main.py
@@ -5,13 +5,19 @@ from app.auto_gui.auto_entry_agent import AutoEntryAgent
 from app.data_formatter.readers.zendesk.zendesk_data_reader import ZendeskDataReader
 from app.data_formatter.formatters.data_formatter_factory import DataFormatterFactory
 from datetime import date
+from PyQt5.QtCore import QObject, pyqtSignal
 
 
-class AutoEntryMain:
+class AutoEntryMain(QObject):
+    started_signal = pyqtSignal()
+    finished_signal = pyqtSignal()
+    exception_signal = pyqtSignal(Exception)
 
     def __init__(self, zendesk_excel_path: str, user_name: str,
                  clear_existing_data: bool, use_fn_key: bool, selected_week: str,
                  num_sap_rows_per_page: int = None) -> None:
+        super().__init__()
+
         self._zendesk_excel_path = zendesk_excel_path
         self._user_name = user_name
         self._num_sap_rows_per_page = num_sap_rows_per_page
@@ -19,36 +25,43 @@ class AutoEntryMain:
         self._use_fn_key = use_fn_key
         self.st, self.ed = selected_week.split(" - ")
 
-    def execute(self):
-        # data formatting
-        zendesk_reader = ZendeskDataReader(self._zendesk_excel_path)
-        zendesk_reader.load_wb()
-        zendesk_rows = zendesk_reader.read_all_rows()
+    def run(self):
+        self.started_signal.emit()
+        try:
+            # data formatting
+            zendesk_reader = ZendeskDataReader(self._zendesk_excel_path)
+            zendesk_reader.load_wb()
+            zendesk_rows = zendesk_reader.read_all_rows()
 
-        # get a formatter
-        formatter_factory = DataFormatterFactory()
-        data_formatter = formatter_factory.get_formatter('ZENDESK')
-        # create pages for a provided month
+            # get a formatter
+            formatter_factory = DataFormatterFactory()
+            data_formatter = formatter_factory.get_formatter('ZENDESK')
+            # create pages for a provided month
 
-        # ***Needs to be changed to use the month of the selected sheet**
-        pages = data_formatter.create_pages(date.today().month)
+            # ***Needs to be changed to use the month of the selected sheet**
+            pages = data_formatter.create_pages(date.today().month)
 
-        # create a new formatter instance with the test data
-        zdt_formatter = data_formatter(self._user_name, zendesk_rows, pages)
-        # format zendesk data to SAP
-        zdt_formatter.format()
+            # create a new formatter instance with the test data
+            zdt_formatter = data_formatter(self._user_name, zendesk_rows, pages)
+            # format zendesk data to SAP
+            zdt_formatter.format()
 
-        # get SAP pages
-        # formatted_data = zdt_formatter.formatted_data()
-        sap_rows = zdt_formatter.get_page(self.st, self.ed).data
+            # get SAP pages
+            # formatted_data = zdt_formatter.formatted_data()
+            sap_rows = zdt_formatter.get_page(self.st, self.ed).data
 
-        if len(sap_rows) == 0:
-            raise Exception('No results were produced for the configured settings.')
-        
-        # auto entry
-        main_wc = factory.get_sap_main_window_controller()
-        main_kc = KeyboardController(main_wc, use_fn_key=self._use_fn_key)
-        main_nav = SapMainWindowNavigator(main_kc, rows_per_page=self._num_sap_rows_per_page)
-        main_wc.set_window_foreground()
-        entry_agent = AutoEntryAgent(main_kc, main_nav, sap_rows)
-        entry_agent.execute(self._clear_existing_data)
+            if len(sap_rows) == 0:
+                raise Exception('No results were produced for the configured settings.')
+            
+            # auto entry
+            main_wc = factory.get_sap_main_window_controller()
+            main_kc = KeyboardController(main_wc, use_fn_key=self._use_fn_key)
+            main_nav = SapMainWindowNavigator(main_kc, rows_per_page=self._num_sap_rows_per_page)
+            main_wc.set_window_foreground()
+            entry_agent = AutoEntryAgent(main_kc, main_nav, sap_rows)
+            entry_agent.execute(self._clear_existing_data)
+
+        except Exception as ex:
+            self.exception_signal.emit(ex)
+
+        self.finished_signal.emit()
--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import QThread
 from PyQt5.QtWidgets import QMessageBox
 from main_window_base import Ui_MainWindow
 from app.auto_entry_main import AutoEntryMain
@@ -12,13 +13,18 @@ from app.data_formatter.readers.zendesk.zendesk_data_reader import ZendeskDataRe
 '''
 class MainWindowFunctional(Ui_MainWindow):
     _not_running_message = 'App is not running'
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.auto_entry_thread = None
+        self.auto_entry_worker = None
+        self.reader = None
     
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
         self.select_data_button.clicked.connect(self.select_data_button_clicked)
         self.username_selector.currentIndexChanged.connect(self.username_changed)
         self.run_button.clicked.connect(self.run_button_clicked)
-        self.reader = None
 
     def select_data_button_clicked(self):
         file_name = self._open_file_selector()
@@ -65,20 +71,38 @@ class MainWindowFunctional(Ui_MainWindow):
         self.week_selector.addItems([start.strftime('%m/%d/%Y') +" - "+ end.strftime('%m/%d/%Y') for start, end in sorted(list(weeks))])
 
     def run_button_clicked(self):
-        print('Running app')
+        print('Running Auto Entry...')
+        self.run_button.setEnabled(False)
+
+        self.auto_entry_thread = QThread()
+        self.auto_entry_worker = AutoEntryMain(
+            self.selected_file_label.text(),
+            self.username_selector.currentText(),
+            self.clear_data_checkbox.isChecked(),
+            self.use_fn_checkbox.isChecked(),
+            self.week_selector.currentText(),
+            self.rows_per_page_box.value())
+
+        self.auto_entry_worker.moveToThread(self.auto_entry_thread)
+        self.auto_entry_thread.started.connect(self.auto_entry_worker.run)
+        self.auto_entry_thread.finished.connect(self.auto_entry_thread.deleteLater)
+        self.auto_entry_worker.started_signal.connect(self._on_auto_entry_started)
+        self.auto_entry_worker.finished_signal.connect(self.auto_entry_thread.quit)
+        self.auto_entry_worker.finished_signal.connect(self.auto_entry_worker.deleteLater)
+        self.auto_entry_worker.finished_signal.connect(self._on_auto_entry_finished)
+        self.auto_entry_worker.exception_signal.connect(self._on_auto_entry_exception)
+        self.auto_entry_thread.start()
+    
+    def _on_auto_entry_started(self):
         self._publish_status_message('App is running')
-        try:
-            AutoEntryMain(self.selected_file_label.text(),
-                    self.username_selector.currentText(),
-                    self.clear_data_checkbox.isChecked(),
-                    self.use_fn_checkbox.isChecked(),
-                    self.week_selector.currentText(),
-                    self.rows_per_page_box.value()
-                    ).execute()
-        except Exception as ex:
-            print(ex)
-            self._show_error_popup(str(ex))
+    
+    def _on_auto_entry_finished(self):
         self._publish_status_message(MainWindowFunctional._not_running_message)
+        self.run_button.setEnabled(True)
+    
+    def _on_auto_entry_exception(self, ex: Exception):
+        print(ex)
+        self._show_error_popup(str(ex))
 
     def _open_file_selector(self):
         fileName, _ = QtWidgets.QFileDialog.getOpenFileName(
