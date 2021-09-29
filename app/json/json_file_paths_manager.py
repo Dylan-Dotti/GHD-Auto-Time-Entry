c4bd223ec3990e2132c6d50af33c8efb341f88d4
Dylan-Dotti <invocation42@yahoo.com>
2021-09-29 14:31:44 -0400

    Update json_file_paths_manager.py

--- app/json/json_file_paths_manager.py
+++ app/json/json_file_paths_manager.py
@@ -2,3 +2,8 @@
 # for use by json_file_factory
 # have some concerns about relative pathing when accessing the json_files directory
 # this would ideally return absolute paths
+
+class JsonFilePathsManager:
+
+    def get_option_prefs_path(self):
+        return ''
