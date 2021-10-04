90c0adfb232cb53ba6bd322a2fd271f54c450cb0
Dylan-Dotti <invocation42@yahoo.com>
2021-10-04 19:25:53 -0400

    JsonFile write_data fix

--- app/json/json_file.py
+++ app/json/json_file.py
@@ -15,16 +15,16 @@ class JsonFile:
                 return json.load(json_path)
         except Exception as err:
             # place hold just kill
-            print("Error reading perf file: " + err)
+            print("Error reading pref file: " + str(err))
             exit(0)    
 
     # overwrites the data in file_path with the given data
     def write_data(self, data: Dict[str, any]):
         try:
             with open (self.file_path, 'w') as json_path:
-                json.dump(data)
+                json.dump(data, json_path, indent=4)
 
         except Exception as err:
            # place hold just kill
-            print("Error reading perf file: " + err)
+            print("Error reading pref file: " + str(err))
             exit(0)   
