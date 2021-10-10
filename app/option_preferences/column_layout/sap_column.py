024582d5d9c667d3366b09614ebf22a9c0311ebc
Dylan-Dotti <invocation42@yahoo.com>
2021-10-10 11:24:19 -0400

    Added interactable params to SapColumn

--- app/option_preferences/column_layout/sap_column.py
+++ app/option_preferences/column_layout/sap_column.py
@@ -3,23 +3,29 @@ from typing import Dict
 
 class SapColumn:
 
-    def __init__(self, column_name: str, enabled: bool, 
-                 enabled_default: bool, default_index: int) -> None:
+    def __init__(self, column_name: str, visible: bool, 
+                 visible_default: bool, interactable: bool, 
+                 noninteractable_str: str, default_index: int) -> None:
         self.column_name = column_name
-        self.enabled = enabled
-        self.enabled_default = enabled_default
+        self.visible = visible
+        self.visible_default = visible_default
+        self.interactable = interactable
+        self.noninteractable_str = noninteractable_str
         self.default_index = default_index
 
     def to_dict(self) -> Dict[str, any]:
         return {
             'column_name': self.column_name,
-            'enabled': self.enabled,
-            'enabled_default': self.enabled_default,
+            'visible': self.visible,
+            'visible_default': self.visible_default,
+            'interactable': self.interactable,
+            'noninteractable_str': self.noninteractable_str,
             'default_index': self.default_index,
         }
 
     @staticmethod
     def from_dict(data: Dict[str, any]) -> 'SapColumn':
         return SapColumn(
-            data['column_name'], data['enabled'],
-            data['enabled_default'], data['default_index'])
+            data['column_name'], data['visible'],
+            data['visible_default'], data['interactable'],
+            data['noninteractable_str'], data['default_index'])
