f840e39cc645b0277009c901501518cb2dbdc966
Dylan-Dotti <invocation42@yahoo.com>
2021-10-15 10:21:37 -0400

    Removed visible_default and default_index from SapColumn

--- app/option_preferences/column_layout/sap_column.py
+++ app/option_preferences/column_layout/sap_column.py
@@ -3,27 +3,23 @@ from typing import Dict
 
 class SapColumn:
 
-    def __init__(self, column_name: str, visible: bool, 
-                 visible_default: bool, interactable: bool, 
-                 default_index: int) -> None:
+    def __init__(self, column_name: str,
+                 visible: bool, interactable: bool) -> None:
         self.column_name = column_name
         self.visible = visible
-        self.visible_default = visible_default
         self.interactable = interactable
-        self.default_index = default_index
 
     def to_dict(self) -> Dict[str, any]:
         return {
             'column_name': self.column_name,
             'visible': self.visible,
-            'visible_default': self.visible_default,
             'interactable': self.interactable,
-            'default_index': self.default_index,
         }
 
     @staticmethod
     def from_dict(data: Dict[str, any]) -> 'SapColumn':
         return SapColumn(
-            data['column_name'], data['visible'],
-            data['visible_default'], data['interactable'],
-            data['default_index'])
+            data['column_name'],
+            data['visible'],
+            data['interactable']
+        )
