d9ee64873614339a48556a704b160154a34aa4c4
Dylan-Dotti <invocation42@yahoo.com>
2022-02-28 19:48:10 -0500

    Print exception when caught during launch_window

--- app/main.py
+++ app/main.py
@@ -5,15 +5,23 @@ from app.option_preferences.option_prefs_json_interface import OptionPrefsJsonIn
 
 def main():
     print('GHD Auto Time Entry, v1.1.1')
+
     option_prefs_interface = OptionPrefsJsonInterface()
+
+    # load option prefs
     try:
         option_prefs = option_prefs_interface.load_option_prefs()
     except:
-        print(traceback.format_exc())
+        print('\n', traceback.format_exc(), '\n')
         option_prefs = None
+
+    # launch app
     try:
         launch_window(option_prefs=option_prefs)
+    except:
+        print('\n', 'App crashed due to exception: ', '\n', traceback.format_exc(), '\n')
     finally:
+        # save option prefs after running app
         try:
             option_prefs_interface.save_option_prefs(option_prefs)
         except:
