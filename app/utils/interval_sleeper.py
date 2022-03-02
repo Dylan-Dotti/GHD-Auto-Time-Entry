908d20f6fb160966ce286bc9f08238aba7c11c61
Dylan-Dotti <invocation42@yahoo.com>
2022-03-02 09:35:40 -0500

    Create interval_sleeper.py

---
+++ app/utils/interval_sleeper.py
@@ -0,0 +1,13 @@
+import time
+
+def interval_sleep(secs: float, interval: float,
+                           continue_callback):
+    start_time = time.time()
+    while True:
+        elapsed = time.time() - start_time
+        if (elapsed >= secs):
+            return
+        if not continue_callback():
+            print("Interval-checked sleep breaking after %ss" % elapsed)
+            return
+        time.sleep(min(interval, secs - elapsed))
